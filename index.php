d8da2816a0b1b9293e38adce8fb3a283acea91c5
Piotr Wojcik <p.p.wojcik@interia.pl>
2022-11-20 19:20:38 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$database_text = 'Hello from backend!';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $database_text; ?>
+</body>
+</html>
